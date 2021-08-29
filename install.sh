c93e69d72eb5aac39d0e1c89c6034b2fa4a3f144
2523615279 <53310530+2523615279@users.noreply.github.com>
2021-08-30 00:44:23 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -802,7 +802,7 @@ install_v2ray() {
 		cp -rf $(pwd)/* /etc/v2ray/233boy/v2ray
 	else
 		pushd /tmp
-		git clone https://github.com/233boy/v2ray -b "$_gitbranch" /etc/v2ray/233boy/v2ray --depth=1
+		git clone -b master https://github.com.cnpmjs.org/233boy/v2ray.git
 		popd
 
 	fi
